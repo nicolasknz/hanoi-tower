cbf46cc73c70766800d1e398bacbc395be2a6f35
Nicolas <nicolas.strike1234@hotmail.com>
2020-06-15 13:19:00 -0300

    Trocar nome das classes das torres e começa a colocar os discos

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const container = document.getElementById("container");
+const torre1 = document.querySelector(".torre1");
+const torre2 = document.querySelector(".torre2");
+const torre3 = document.querySelector(".torre3");
 
 container.addEventListener ("click" , function(e) {
-
-
-    if (e.target.classList.contains("torreUm")) {
+    let estado = 0;
+    if (e.target.classList.contains("torre1")) {
         console.log("Torre um");
+        estado = 1;
     }
 
-    if (e.target.classList.contains("torreDois")) {
+    if (e.target.classList.contains("torre2")) {
         console.log("Torre dois");
+        estado = 1;
     }
 
-    if (e.target.classList.contains("torreTres")) {
+    if (e.target.classList.contains("torre3")) {
         console.log("Torre tres");
+        estado = 1;
     }
 })
+
+let disco1 = document.createElement("div");
+disco1.classList.add("disco1");
+
+torre1.appendChild(disco1);
+
+
+
+
