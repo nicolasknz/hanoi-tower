e68b2e05abdecdcd60678b1b49d3cc212615871b
Nicolas <nicolas.strike1234@hotmail.com>
2020-06-16 15:59:46 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const container = document.getElementById("container");
 const torre1 = document.querySelector(".torre1");
 const torre2 = document.querySelector(".torre2");
@@ -7,75 +8,180 @@ let estado = 1;
 let qualTorre = 0;
 let ultimoDisco;
 
-container.addEventListener ("click" , function(e) {
-    if (ultimoDisco === null) {
-        estado++
-    }
-    if (e.target.classList.contains("torre1")) {
-        if( estado % 2 !== 0) {
-            estado++;
-            ultimoDisco = torre1.lastElementChild;
-        } else if ( estado % 2 === 0) {
-            if (ultimoDisco !== null && ultimoDisco !== undefined ){
-                if (widthSize(ultimoDisco,torre1.lastElementChild) === true){
-                    torre1.appendChild(ultimoDisco);
-                    estado++;
+function criarTorres () {
+    let containerTorre = document.createElement("div");
+    containerTorre.classList.add("containerTorre");
+    container.appendChild(containerTorre);
+    let torre1 = document.createElement("div");
+    torre1.classList.add("torre1")
+    containerTorre.appendChild(torre1);
+
+    let containerTorre2 = document.createElement("div");
+    containerTorre2.classList.add("containerTorre");
+    container.appendChild(containerTorre2);
+    let torre2 = document.createElement("div");
+    torre2.classList.add("torre2");
+    containerTorre2.appendChild(torre2);
+
+    let containerTorre3 = document.createElement("div");
+    containerTorre3.classList.add("containerTorre");
+    container.appendChild(containerTorre3);
+    let torre3 = document.createElement("div");
+    torre3.classList.add("torre3");
+    containerTorre3.appendChild(torre3);
+
+    criarDiscos(torre1);
+
+
+    container.addEventListener ("click" , function(e) {
+
+        if (ultimoDisco === null) {
+            estado++
+        }
+        if (e.target.classList.contains("torre1")) {
+            if( estado % 2 !== 0) {
+                estado++;
+                ultimoDisco = torre1.lastElementChild;
+            } else if ( estado % 2 === 0) {
+                if (ultimoDisco !== null && ultimoDisco !== undefined ){
+                    if (widthSize(ultimoDisco,torre1.lastElementChild) === true){
+                        torre1.appendChild(ultimoDisco);
+                        estado++;
+                } else {
+                    
+                    ultimoDisco = null;
+                }
+            }
             }
         }
-        }
-    }
-    if (e.target.classList.contains("torre2")) {
-        if( estado % 2 !== 0) {
-            ultimoDisco = torre2.lastElementChild;
-            estado++;
-        } else if ( estado % 2 === 0) {
-            if (ultimoDisco !== null && ultimoDisco !== undefined){
-                if(widthSize(ultimoDisco,torre2.lastElementChild) === true) {
-                    torre2.appendChild(ultimoDisco);
-                    estado++;    
+        if (e.target.classList.contains("torre2")) {
+            if( estado % 2 !== 0) {
+                ultimoDisco = torre2.lastElementChild;
+                estado++;
+            } else if ( estado % 2 === 0) {
+                if (ultimoDisco !== null && ultimoDisco !== undefined){
+                    if(widthSize(ultimoDisco,torre2.lastElementChild) === true) {
+                        torre2.appendChild(ultimoDisco);
+                        estado++;    
+                } else {
+                    
+                    ultimoDisco = null;
+                }
+            }   
             }
-        }   
         }
-    }
-    if (e.target.classList.contains("torre3")) {
+        if (e.target.classList.contains("torre3")) {
+            if( estado % 2 !== 0) {
+                ultimoDisco = torre3.lastElementChild;
+                estado++;
+            }  else if ( estado % 2 === 0) {
+                if(ultimoDisco !== null && ultimoDisco !== undefined){
+                    if(widthSize(ultimoDisco,torre3.lastElementChild) === true){
+                        torre3.appendChild(ultimoDisco);
+                        estado++;
+                } else {
+                
+                    ultimoDisco = null;
+                }
+            }
+            }
+
+            determinarVitoria(torre3);
+        }
+        pegandoSoltado(estado);
     
-        if( estado % 2 !== 0) {
-            ultimoDisco = torre3.lastElementChild;
-            estado++;
-        }  else if ( estado % 2 === 0) {
-            if(ultimoDisco !== null && ultimoDisco !== undefined){
-                if(widthSize(ultimoDisco,torre3.lastElementChild) === true){
-                    torre3.appendChild(ultimoDisco);
-                    estado++;
-            }
-        }
-        }
+    })
+}
+criarTorres();
 
-        determinarVitoria(torre3);
-    }
+function criarDiscos(torre1){
+    const discoDinamico1 = document.createElement("div");
+    discoDinamico1.id = "disco1";
+    torre1.appendChild(discoDinamico1);
 
-    pegandoSoltado(estado);
-    console.log(ultimoDisco);
-})
+    const discoDinamico2 = document.createElement("div");
+    discoDinamico2.id = "disco2";
+    torre1.appendChild(discoDinamico2);
 
-const discoDinamico1 = document.createElement("div");
-discoDinamico1.id = "disco1";
-torre1.appendChild(discoDinamico1);
+    const discoDinamico3 = document.createElement("div");
+    discoDinamico3.id = "disco3";
+    torre1.appendChild(discoDinamico3);
 
-const discoDinamico2 = document.createElement("div");
-discoDinamico2.id = "disco2";
-torre1.appendChild(discoDinamico2);
+    const discoDinamico4 = document.createElement("div");
+    discoDinamico4.id = "disco4";
+    torre1.appendChild(discoDinamico4);
+}
 
-const discoDinamico3 = document.createElement("div");
-discoDinamico3.id = "disco3";
-torre1.appendChild(discoDinamico3);
+// container.addEventListener ("click" , function(e) {
+//     console.log(torre1);
 
-const discoDinamico4 = document.createElement("div");
-discoDinamico4.id = "disco4";
-torre1.appendChild(discoDinamico4);
+//     if (ultimoDisco === null) {
+//         estado++
+//     }
+//     if (e.target.classList.contains("torre1")) {
+//         if( estado % 2 !== 0) {
+//             estado++;
+//             console.log(torre1.lastElementChild);
+//             ultimoDisco = torre1.lastElementChild;
+//         } else if ( estado % 2 === 0) {
+//             if (ultimoDisco !== null && ultimoDisco !== undefined ){
+//                 if (widthSize(ultimoDisco,torre1.lastElementChild) === true){
+//                     torre1.appendChild(ultimoDisco);
+//                     estado++;
+//             }
+//         }
+//         }
+//     }
+//     if (e.target.classList.contains("torre2")) {
+//         if( estado % 2 !== 0) {
+//             ultimoDisco = torre2.lastElementChild;
+//             estado++;
+//         } else if ( estado % 2 === 0) {
+//             if (ultimoDisco !== null && ultimoDisco !== undefined){
+//                 if(widthSize(ultimoDisco,torre2.lastElementChild) === true) {
+//                     torre2.appendChild(ultimoDisco);
+//                     estado++;    
+//             }
+//         }   
+//         }
+//     }
+//     if (e.target.classList.contains("torre3")) {
+//         if( estado % 2 !== 0) {
+//             ultimoDisco = torre3.lastElementChild;
+//             estado++;
+//         }  else if ( estado % 2 === 0) {
+//             if(ultimoDisco !== null && ultimoDisco !== undefined){
+//                 if(widthSize(ultimoDisco,torre3.lastElementChild) === true){
+//                     torre3.appendChild(ultimoDisco);
+//                     estado++;
+//             }
+//         }
+//         }
 
+//         determinarVitoria(torre3);
+//     }
+//     pegandoSoltado(estado);
+   
+// })
+
+// const discoDinamico1 = document.createElement("div");
+// discoDinamico1.id = "disco1";
+// torre1.appendChild(discoDinamico1);
+
+// const discoDinamico2 = document.createElement("div");
+// discoDinamico2.id = "disco2";
+// torre1.appendChild(discoDinamico2);
+
+// const discoDinamico3 = document.createElement("div");
+// discoDinamico3.id = "disco3";
+// torre1.appendChild(discoDinamico3);
+
+// const discoDinamico4 = document.createElement("div");
+// discoDinamico4.id = "disco4";
+// torre1.appendChild(discoDinamico4);
+
+// Testa se o disco que vai soltar é menor que o ultimo disco da torre em que sera colocado
 function widthSize (pegou,soltou) {
-  
     if( soltou === null){
         return true;
     }
@@ -86,9 +192,10 @@ function widthSize (pegou,soltou) {
         return true;
     } else if (pegou.clientWidth > soltou.clientWidth) {
         alert("movimento impossivel");
-        false;
+        return false;
     }
 }
+// Mostra em qual modo o usuario esta
 function pegandoSoltado () {
     state = document.getElementById("state");
     state.textContent = "Pegue um disco";
@@ -97,13 +204,15 @@ function pegandoSoltado () {
         state.classList.add ("pegando");
     } else if ( estado % 2 === 0) {
         state.textContent = "Solte o disco";
+        state.classList.remove("pegando");
         state.classList.add ("soltando");
     }
 }
 function determinarVitoria(nomeTorre){
     if(nomeTorre.childElementCount === 4){
         alert("Vitória");
-        
         return true;
     }
 }
+
+
